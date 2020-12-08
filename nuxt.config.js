d4b797aa4cb49588d7b8bc6f75e42b19c38ff2b5
Jessica Tarra <jessictarra@gmail.com>
2020-12-07 21:06:46 -0300

    adding color mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,12 +24,12 @@ export default {
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
     // dark-mode
     '@nuxtjs/color-mode',
+    //svg-nuxt
+    '@nuxtjs/svg'
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
