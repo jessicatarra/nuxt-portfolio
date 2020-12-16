6700a6ed1250cf9f4efcaf55cb55bcb3adc413bc
Jessica Tarra <jessictarra@gmail.com>
2020-12-16 20:46:51 -0300

    another day

---
+++ components/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="px-8 pt-24 pb-8 flex flex-col justify-center space-y-8">
+        <h1 class="text-center text-2xl md:text-3xl">Hi, I'm Jessica Tarra 👋</h1>
+        <vue-typer  class="h-22 md:h-48 text-4xl md:text-6xl font-bold text-center" text='Hello World! I was registered locally!'></vue-typer>
+        <h1 class="text-center text-xl md:text-2xl">I'm a <strong class="font-bold">Front-end Developer </strong>from Cartagena, Colombia and currently living in Rio de Janeiro, Brazil.</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+    .vue-typer .custom.char.typed {
+    color: var(--color);
+    }
+</style>
+
---
+++ components/Menu.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="menu absolute w-full z-10">
+        <ul class="flex flex-col space-y-4 mx-8 sm:mt-4 sm:py-1 mt-24 py-2 mb-12">
+            <li class="menu-item flex flex-row justify-start space-x-4"
+            >
+                <IconHome/>
+                <a class="font-bold tracking-tight" href="#">Home</a>
+            </li>
+            <li class="menu-item flex flex-row justify-start space-x-4"
+            >
+                <IconAbout />
+                <a class="font-bold tracking-tight" href="#">About</a>
+            </li>
+            <li class="menu-item flex flex-row justify-start space-x-4"
+            >
+                <IconServices />
+                <a class="font-bold tracking-tight" href="#">Services</a>
+            </li>
+            <li class="menu-item flex flex-row justify-start space-x-4"
+            >
+                <IconExperience />
+                <a class="font-bold tracking-tight" href="#">Experience</a>
+
+            </li>
+            <li class="menu-item flex flex-row justify-start space-x-4"
+            >
+                <IconCases />
+                <a class="font-bold tracking-tight" href="#">Cases</a>
+            </li>
+            <li class="menu-item flex flex-row justify-start space-x-4"
+            >
+                <IconBlog />
+                <a class="font-bold tracking-tight" href="#">Blog</a>
+            </li>
+            <li class="menu-item flex flex-row justify-start space-x-4"
+            >
+                <IconContact />
+                <a class="font-bold tracking-tight" href="#">Contact</a>
+            
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import IconHome from '@/assets/icons/home.svg?inline'
+import IconAbout from '@/assets/icons/about.svg?inline'
+import IconServices from '@/assets/icons/services.svg?inline'
+import IconExperience from '@/assets/icons/experience.svg?inline'
+import IconCases from '@/assets/icons/cases.svg?inline'
+import IconBlog from '@/assets/icons/blog.svg?inline'
+import IconContact from '@/assets/icons/contact.svg?inline'
+
+export default {
+    
+    components: {
+        IconHome,
+        IconAbout,
+        IconServices,
+        IconExperience,
+        IconCases,
+        IconBlog,
+        IconContact,
+    },
+   data() {
+        return  { 
+
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.menu {
+    background-color: var(--bg);
+}
+.svg-inline--fa {
+    cursor: pointer;
+}
+
+.menu-item:hover> *, .menu-item:active> *{
+    color: var(--color-primary);
+    transition: 0.3s;
+}
+</style>
