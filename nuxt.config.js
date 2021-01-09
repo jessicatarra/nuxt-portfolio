9597dbc2ec00068f9c30e21bf69b517a098edc83
Jessica Tarra <jessictarra@gmail.com>
2021-01-09 13:33:54 -0300

    Update nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,7 @@ export default {
       {
         hid: 'twitter:image',
         name: 'twitter:image',
-        content: 'assets/images/jessica.jpg'
+        content: '/assets/images/jessica.jpg'
       },
       { 
         hid: 'description', 
@@ -58,7 +58,7 @@ export default {
       {
         hid: 'og:image',
         property: 'og:image',
-        content: 'assets/images/jessica.jpg'
+        content: '/assets/images/jessica.jpg'
       },
       {
         hid: 'og:site_name',
